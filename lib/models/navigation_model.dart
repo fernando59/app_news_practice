18ea3e31a505c8a5f50181474279afc968182a3b
Fernando Mercado <fernandomercado529@gmail.com>
2022-11-23 18:20:37 -0400

    Add provider to management state

---
+++ lib/models/navigation_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class NavigationModel with ChangeNotifier {
+  int _currentPage = 0;
+
+  int get currentPage => _currentPage;
+
+  set currentPage(int value) {
+    _currentPage = value;
+    notifyListeners();
+  }
+}
--- lib/pages/tabs_screen.dart
+++ lib/pages/tabs_screen.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/models/navigation_model.dart';
+import 'package:provider/provider.dart';
 
 class TabScreen extends StatelessWidget {
   const TabScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: PageView(
-      physics: const BouncingScrollPhysics(),
+    return ChangeNotifierProvider(
+      create: (_) => NavigationModel(),
+      child: const Scaffold(
+        body: _Pages(),
+        bottomNavigationBar: _Navigation(),
+      ),
+    );
+  }
+}
+
+class _Navigation extends StatelessWidget {
+  const _Navigation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navigationModel = Provider.of<NavigationModel>(context);
+
+    return BottomNavigationBar(
+      currentIndex: navigationModel.currentPage,
+      onTap: (i) => navigationModel.currentPage = i,
+      items: const [
+        BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline), label: 'For You'),
+        BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Header'),
+      ],
+    );
+  }
+}
+
+class _Pages extends StatelessWidget {
+  const _Pages({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const NeverScrollableScrollPhysics(),
       children: <Widget>[
         Container(
           color: Colors.red,
@@ -16,6 +55,6 @@ class TabScreen extends StatelessWidget {
           color: Colors.green,
         )
       ],
-    ));
+    );
   }
 }
